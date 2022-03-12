496ed998a1a5a53ca6737c5ebed2a9ea772e4c3e
Jaqueline Lin <XJACKIECO@gmail.com>
2022-03-11 23:15:25 -0500

    Update index.html

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,29 @@
+// Create a close button and append it to each list item
+
+const myNodelist = document.getElementsByTagName("li");
+for (i = 0; i < myNodelist.length; i++) {
+    const span = document.createElement("spam");
+    const txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.append(txt);
+    myNodelist[i].append(span);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function ramdomRgb() {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
@@ -8,4 +34,9 @@ function ramdomRgb() {
 
 const letters = document.getElementsByClassName('letter')
 
-letters.style.color = ramdomRgb();
+console.log(letters)
+
+
+
+
+letters.style.color = "blue";

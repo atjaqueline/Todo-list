f920136db78dc8ab9c05cec4bd14efb543cb8721
Jaqueline Lin <XJACKIECO@gmail.com>
2022-03-12 21:32:49 -0500

    Update index.html

--- JS/app.js
+++ JS/app.js
@@ -21,6 +21,49 @@ for (let i = 0; i < close.length; i++) {
     });
 }
 
+// Add class "Checked" when list is clicked
+
+const listItem = document.querySelector("ul");
+
+
+listItem.addEventListener('click', function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+});
+
+
+// Create a new list item when clicking "Add" button
+
+const addItem = document.querySelector(".addBtn");
+
+addItem.addEventListener('click', function() {
+    const li = document.createElement("li")
+    const inputValue = document.getElementById("myInput").value;
+    const t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must enter a task!");
+    } else {
+        document.getElementById("myUL").append(li);
+    }
+    document.getElementById("myInput").value = "";
+    const span = document.createElement("spam");
+    const txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.append(txt);
+    li.append(span);
+
+    let close = document.getElementsByClassName("close");
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].addEventListener("click", function() {
+            myNodelist[i].style.display = "none";
+
+        })
+    }
+});
+
 
 
 
